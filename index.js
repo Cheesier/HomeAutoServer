4d14ea0c32b3d82839bc4c5cabadff6a1fad55ab
Oscar Thunberg <ostenip@gmail.com>
2017-07-04 21:31:01 +0200

    Allow console interactivity

--- index.js
+++ index.js
@@ -74,3 +74,16 @@ app.get('/pair/:id', function (req, res) {
   sendMessage(`PAIR ${req.params.id}`)
   res.send(buttons)
 })
+
+
+
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(d) {
+    // note:  d is an object, and when converted to a string it will
+    // end with a linefeed.  so we (rather crudely) account for that
+    // with toString() and then trim()
+    const msg = d.toString().trim();
+    console.log("console: [" + msg + "]");
+    sendMessage(msg);
+  });
