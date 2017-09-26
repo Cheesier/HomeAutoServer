6e6243aa553232796be8886169418e9eb7be0aa6
Oscar Thunberg <ostenip@gmail.com>
2017-09-26 22:59:58 +0200

    New housecode to work with telldus devices

--- devices.js
+++ devices.js
@@ -4,21 +4,23 @@ const cron = require('node-cron')
 let lights = {}
 //let tasks = {}
 
-addNexaLight(lights, 1337, 'Tv', 1000, 0, [
+let houseCode = 1510
+
+addNexaLight(lights, 1337, 'Tv', houseCode, 0, [
     nexaRemoteButton(2471582, 0),
     nexaRemoteButton(2259722, 0)
 ])
-addNexaLight(lights, 1338, 'Fönster', 1000, 1,  [
+addNexaLight(lights, 1338, 'Fönster', houseCode, 1,  [
     nexaRemoteButton(2471582, 1),
     nexaRemoteButton(2259722, 1),
     nexaRemoteButton(23047482, 10) // Wall switch, right button
 ])
-addNexaLight(lights, 1339, 'Säng', 1000, 2, [
+addNexaLight(lights, 1339, 'Säng', houseCode, 2, [
     nexaRemoteButton(2471582, 2),
     nexaRemoteButton(2259722, 2)
 ])
-addNexaLight(lights, 1340, 'Kontor', 1000, 3, [])
-addNexaLight(lights, 1341, 'Kökslampa', 1000, 4, [
+addNexaLight(lights, 1340, 'Kontor', houseCode, 3, [])
+addNexaLight(lights, 1341, 'Kökslampa', houseCode, 4, [
     nexaRemoteButton(23047482, 11) // Wall switch, left button
 ])
 
